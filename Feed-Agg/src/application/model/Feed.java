57269040d182bfad907759341ca0655f971ca789
Seth Chick <SCBionicle@gmail.com>
2018-03-31 12:17:37 -0500

    adding feed manager

---
+++ Feed-Agg/src/application/model/Feed.java
@@ -0,0 +1,21 @@
+package application.model;
+
+import java.net.URL;
+
+/**
+ * Manages feed from one URL
+ * @author Seth Chick, hnn727
+ *
+ */
+public class Feed {
+
+    private URL url;
+    
+    
+    public Feed(URL url) {
+	
+    }
+    
+    
+    
+}
