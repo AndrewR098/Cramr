d4fbd9ee6a747bfc5ceff25cfbe928cd152870f0
Seth Chick <SCBionicle@gmail.com>
2018-04-05 13:33:14 -0500

    added junit

---
+++ Feed-Agg/src/FeedTest/FeedTest.java
@@ -0,0 +1,24 @@
+package FeedTest;
+
+import static org.junit.Assert.*;
+
+import java.net.URL;
+
+import org.junit.Test;
+
+import application.model.Feed;
+
+public class FeedTest {
+
+    @Test
+    public void testFeed() throws Exception {
+	//try {
+	    Feed feed = new Feed(new URL("http://rss.cnn.com/rss/cnn_topstories.rsss"));
+	//}catch(Exception e){
+	//    e.printStackTrace();
+	//    fail("Not yet implemented");
+	//}
+	
+    }
+
+}
