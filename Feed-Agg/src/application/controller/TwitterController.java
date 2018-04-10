35c0d979c29270146cda4bb1b80b6e6371bf74b1
Andrew Rodriguez <andrew.m.rodriguez.000@gmail.com>
2018-04-10 15:34:04 -0500

    Created Twitter Controller with some bare classes.

---
+++ Feed-Agg/src/application/controller/TwitterController.java
@@ -0,0 +1,13 @@
+package application.controller;
+
+public class TwitterController {
+	
+	public void initialize(){
+		//TODO: Get the user's timeline. Load into a text box in the twitter view.
+		
+	}
+	
+	public void postStatus(){
+		//TODO: Get the text from the textfield. Post it using Twitter4J libraries.
+	}
+}
