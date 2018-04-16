9b16edda2a9987aee3183fe3f4e5ecf12470296b
Seth Chick <SCBionicle@gmail.com>
2018-04-16 14:10:13 -0500

    making rss test

---
+++ Feed-Agg/src/application/controller/RSSTestController.java
@@ -0,0 +1,24 @@
+package application.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
+
+public class RSSTestController implements Initializable {
+
+	@FXML
+	ImageView image;
+	@FXML
+	ListView<String> list;
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
