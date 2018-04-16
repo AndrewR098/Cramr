ef595d46ed99f1acd4c812cca8b32b4739f4d225
Seth Chick <SCBionicle@gmail.com>
2018-04-16 14:21:18 -0500

    added asynctask

--- Feed-Agg/src/application/controller/RSSTestController.java
+++ Feed-Agg/src/application/controller/RSSTestController.java
@@ -1,8 +1,14 @@
 package application.controller;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import com.rometools.rome.io.FeedException;
+import com.victorlaerte.asynctask.AsyncTask;
+
+import application.model.Feed;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
@@ -10,6 +16,7 @@ import javafx.scene.image.ImageView;
 
 public class RSSTestController implements Initializable {
 
+	Feed feed;
 	@FXML
 	ImageView image;
 	@FXML
@@ -18,7 +25,36 @@ public class RSSTestController implements Initializable {
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
-
+		AsyncTask task = new AsyncTask(){
+			public void doInBackground(){
+				try{
+					feed = new Feed(new URL("http://rss.cnn.com/rss/cnn_topstories.rss"),0);
+				}
+				catch(MalformedURLException e){
+					e.printStackTrace();
+					
+				} catch (IllegalArgumentException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (FeedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			public void progressCallback(Object... params){
+				
+			}
+			public void onPreExecute(){
+				
+			}
+			public void onPostExecute(){
+				//TODO: update views
+			}
+		};
+		task.execute();
 	}
 
 }
