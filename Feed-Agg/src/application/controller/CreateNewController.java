b5e2879fa568af209cc3c20869eb3d0c95dde075
Casey Hammond <casey.j.hammond@gmail.com>
2018-04-18 16:55:01 -0500

    Added functionality for creating new logins.

--- Feed-Agg/src/application/controller/CreateNewController.java
+++ Feed-Agg/src/application/controller/CreateNewController.java
@@ -1,19 +1,63 @@
 package application.controller;
 
-import javafx.event.Event;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
+import java.io.*;
+
+import javafx.event.*;
+import javafx.fxml.*;
+import javafx.scene.*;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class CreateNewController {
+	
 	@FXML 
 	Button homeButton;
+	@FXML
+	TextField fieldEmailOrUser;
+	@FXML
+	PasswordField fieldPassword;
+	@FXML
+	PasswordField fieldConfirmPassword;
+	@FXML
+	Button buttonCreateNew;
 	
 	public void handle(Event event) {
+		if ( fieldPassword.getText().equals( fieldConfirmPassword.getText() ) ) {
+			BufferedWriter bw = null;
+			FileWriter fw = null;
+			try {
+				String data = "\n" + fieldEmailOrUser.getText() + "," + fieldPassword.getText();
+				
+				File file = new File("users.txt");
+				
+				// if file does not exist, create the file.
+				if (!file.exists()) {
+					file.createNewFile();
+				}
+				fw = new FileWriter(file.getAbsoluteFile(), true);
+				bw = new BufferedWriter(fw);
+				
+				bw.write(data);
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					if (bw != null) {
+						bw.close();
+					}
+					if (fw != null) {
+						fw.close();
+					}
+				} catch (IOException ex) {
+					ex.printStackTrace();
+				}
+			}
+			
 			try {
 				Parent root = FXMLLoader.load(getClass().getResource("/Splash.fxml"));
 				Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
@@ -21,6 +65,11 @@ public class CreateNewController {
 				stage.show();
 			} catch(Exception e) {
 				e.printStackTrace();
-			} 
+			}
+			
+		} else {
+			// users passwords didn't match.
+			System.out.println("Passwords do not match, please try again.\n");
+		}
 	}
 }
