4c9b413f3b98eb616c9b4b14d3ecd99575015ea2
Seth Chick <SCBionicle@gmail.com>
2018-04-23 14:25:27 -0500

    implemented reddit functionality

--- Feed-Agg/src/application/controller/HomeController.java
+++ Feed-Agg/src/application/controller/HomeController.java
@@ -1,13 +1,32 @@
 package application.controller;
 
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
+import com.rometools.rome.feed.synd.SyndEntry;
+import com.rometools.rome.io.FeedException;
+import com.victorlaerte.asynctask.AsyncTask;
+
+import application.Main;
+import application.model.Feed;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 /**
  * @author Sam Carey
@@ -19,7 +38,7 @@ import javafx.stage.Stage;
 
  */
 
-public class HomeController{
+public class HomeController implements Initializable{
 	@FXML
 	Button facebookButton;
 	@FXML
@@ -30,6 +49,16 @@ public class HomeController{
 	Button addSocialButton;
 	@FXML
 	Button redditButton;
+	
+	/////////////////REDDIT//////////////////////
+	@FXML
+	ListView<String> rdtFeed;
+	
+	ArrayList<Feed> rFeeds;
+	List<SyndEntry> rMessages;
+	ArrayList<String> rMessageContent;
+	
+	////////////////REDDIT: END//////////////////
 	
 	//@FXML
 	//hyperlink for NewAccount
@@ -99,6 +128,95 @@ public class HomeController{
 		
 	}
 	
+	
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		//Reddit/////////////////////////////////////
+		rFeeds = new ArrayList<Feed>();
+		rMessages = new ArrayList<SyndEntry>();
+		rMessageContent= new ArrayList<String>();
+		File file = new File("subreddits" + LoginController.currentUser.user + ".txt");
+		
+		if (!file.exists())
+			System.out.println("file doesn't exist");
+		
+		Scanner scan;
+		
+		try {
+			scan = new Scanner(file);
+			while (scan.hasNextLine()) {
+				final String currentSubreddit = "https://www.reddit.com/r/" + scan.nextLine() + "/.rss";
+				AsyncTask task = new AsyncTask(){
+				    Feed feed;
+					public void doInBackground(){
+						
+						try{
+							feed = new Feed(new URL(currentSubreddit),0);
+						} catch(MalformedURLException e){
+							e.printStackTrace();
+						} catch (IllegalArgumentException e) {
+							e.printStackTrace();
+						} catch (FeedException e) {
+							e.printStackTrace();
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					}
+					public void progressCallback(Object... params){
+						
+					}
+					public void onPreExecute(){
+						
+					}
+					public void onPostExecute(){
+						//TODO: update views
+						updateFeed(feed);
+					}
+				};
+				task.execute();
+			}
+			scan.close();
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		}
+		//////////////////REDDIT: END///////////////////////////
+		
+	}
+	//////////////////////////REDDIT START///////////////////////
+	/**
+	 * Adds to the Reddit feed display
+	 * @param feed current feed to pass
+	 */
+	private synchronized void updateFeed(Feed feed){
+		//ObservableList<String> oList = FXCollections.observableArrayList();
+		//String feedTitle = feed.getEntries().get(0).getTitle();
+		//oList.add(feedTitle);
+		//subreddits.getItems().add(feedTitle);
+	    List<SyndEntry> mess = feed.getEntries(); //get local entries from feed
+	    List<String> content = new ArrayList<String>(); //make String list for ListView
+	    
+	    rFeeds.add(feed);
+	    for(int i = 0; i<mess.size(); i++) {
+		rMessages.add(mess.get(i));
+		content.add(mess.get(i).getTitle());
+		rMessageContent.add(mess.get(i).getTitle());
+	    }
+	    
+	    rdtFeed.getItems().setAll(rMessageContent);
+	    
+	}
+	/**
+	 * Opens clicked reddit feed
+	 * @param event unused
+	 */
+	public void onRedditClick(MouseEvent event){
+		int selection2 = rdtFeed.getSelectionModel().getSelectedIndex();
+		if(selection2>=0)
+			Main.openWebpageExternal(rMessages.get(selection2).getLink());
+	}
+	///////////////////REDDIT END////////////////////////////////
 }
 
 	
