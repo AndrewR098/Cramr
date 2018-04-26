5d2e83e6d434bb35d7073b95d2b28ee7abda305f
Andrew Rodriguez <andrew.m.rodriguez.000@gmail.com>
2018-04-26 17:46:55 -0500

    Figured out how to properly implement twitter methods and authorization.

--- Feed-Agg/src/application/controller/TwitterController.java
+++ Feed-Agg/src/application/controller/TwitterController.java
@@ -1,5 +1,6 @@
 package application.controller;
 
+import application.model.Item;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -7,6 +8,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextArea;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -15,12 +17,17 @@ import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
 
 public class TwitterController {
 	@FXML Button homeButton;
 	@FXML TextArea userStatus;
-	@FXML Text statusOne, statusTwo, statusThree, statusFour, statusFive, statusSix, statusSeven, statusEight, statusNine, statusTen, statusEleven, statusTwelve, statusThirteen, statusFourteen;
-	private static Twitter usertwitter = new TwitterFactory().getInstance();
+	@FXML TableView<Status> timelineView;
+//	@FXML TableColumn<Status, Double> Column; 
+	@FXML TableColumn<Status, String> userColumn;
+	@FXML TableColumn<Status, String> tweetColumn;
+	private static Twitter usertwitter;
+	private static TwitterFactory tf;
 	
 	
 	public void handle(Event event) {
@@ -48,11 +55,20 @@ public class TwitterController {
 	}
 	
 	public void initialize(){
+		//THIS WILL NOT DO ANYTHING. I am keeping my keys secret, thanks.
+/*		ConfigurationBuilder cb = new ConfigurationBuilder();
+		cb.setDebugEnabled(true)
+		  .setOAuthConsumerKey("QaUhgYDbP3mOQR8DZLgK9RZfa")
+		  .setOAuthConsumerSecret("U58SF3hL5eKoWXoCa9AE7dR0CO25aLH8bDB2KIsUtZuNUu5Mvm")
+
+		this.tf = new TwitterFactory(cb.build());
+		this.usertwitter = tf.getInstance();
 		try {
 			ResponseList<Status> userHome = usertwitter.getHomeTimeline(); // Take this and display it in the status text fields.
+			
 		} catch (TwitterException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		}  */
 	}
 }
