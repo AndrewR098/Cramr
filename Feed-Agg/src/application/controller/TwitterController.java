93dcbb5efa78ba434b59b9f25870be55c11e04c0
Cristian Cisneros <cristiancisneros414@gmail.com>
2018-04-27 14:33:11 -0500

    deleted unnecessary code

--- Feed-Agg/src/application/controller/TwitterController.java
+++ Feed-Agg/src/application/controller/TwitterController.java
@@ -49,16 +49,6 @@ public class TwitterController {
 	private static AccessToken userToken;
 	
 	public void initialize(){
-	/*	ConfigurationBuilder cb = new ConfigurationBuilder();
-		cb.setDebugEnabled(true)
-		  .setOAuthConsumerKey("QaUhgYDbP3mOQR8DZLgK9RZfa")
-		  .setOAuthConsumerSecret("U58SF3hL5eKoWXoCa9AE7dR0CO25aLH8bDB2KIsUtZuNUu5Mvm")
-		  .setOAuthAccessToken("1644016068-Rk73WdeISOMMYbMLtHVeiyGPyQRapiHlKaXEZzu")
-		  .setOAuthAccessTokenSecret("5IL1cNR0MOLzxwm56vCxTcQ0EudjIztcW9d2uqanDJejw");
-
-
-		this.tf = new TwitterFactory(cb.build());
-		this.usertwitter = tf.getInstance(); */
 		try {
 			ResponseList<Status> userHome = usertwitter.getHomeTimeline(); // Take this and display it in the status text fields.
 			this.curTimeline = userHome;
